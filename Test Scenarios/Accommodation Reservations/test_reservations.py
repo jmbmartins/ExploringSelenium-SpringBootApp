fe2bebac3d260b43d01fd4dac83f4497f078b3b9
João Martins <jmbaltazarmartins@gmail.com>
2023-11-04 19:54:12 +0000

    negative case

--- Test Scenarios/Accommodation Reservations/test_reservations.py
+++ Test Scenarios/Accommodation Reservations/test_reservations.py
@@ -164,19 +164,73 @@ def delete_reservation():
 
 
 def checkIn_after_checkOut():
-    pass
+    try:
+        # Login
+        driver.get('http://localhost:8080/')
 
+        email_input = driver.find_element(By.NAME, 'email')
+        password_input = driver.find_element(By.NAME, 'password')
+        login_button = driver.find_element(By.ID, 'btnSub')
 
-def checkIn_before_currentDate():
-    pass
+        email_input.send_keys('user1@example.com')
+        password_input.send_keys('password1')
+        login_button.click()
+
+        time.sleep(2)
+
+        driver.implicitly_wait(10)
+
+        # Home Page
+        btnBooking = driver.find_element(By.ID, 'btnbooking')
+        btnBooking.click()
+
+        # Booking Page
+        btnReserve = driver.find_element(By.CLASS_NAME, "reserve_link")
+        btnReserve.click()
+
+        # Locate the form elements and interact with them
+        check_in_date = driver.find_element(By.CLASS_NAME, "checkInDate")
+        check_out_date = driver.find_element(By.CLASS_NAME, "checkOutDate")
+
+        # Clear any existing date values in the input fields
+        check_in_date.clear()
+        check_out_date.clear()
+
+        # Values to insert
+        checkInDate = "2023-11-29"
+        checkOutDate = "2023-11-19"
+        petName = "Thor"
+        roomType = "standard"
+
+        check_in_date.send_keys(datetime.strptime(checkInDate, "%Y-%m-%d").strftime("%d-%m-%Y"))
+        check_out_date.send_keys(datetime.strptime(checkOutDate, "%Y-%m-%d").strftime("%d-%m-%Y"))
+
+        pet_dropdown = Select(driver.find_element(By.ID, "petId"))
+        pet_dropdown.select_by_visible_text(petName)  # Replace with the desired pet name "Thor"
+
+        room_type_select = Select(driver.find_element(By.ID, "roomTypeSelect"))
+        room_type_select.select_by_value(roomType)  # Replace with your desired room type
+
+        time.sleep(10)
+
+        # Submit the form
+        submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+        submit_button.click()
+
+        # Check for the presence of the error message element
+        error_message_elements = driver.find_elements(By.XPATH, "//div[@class='alert alert-danger']")
+
+        assert error_message_elements
+        print("Test checkIn after checkOut succeeded")
+    except Exception as e:
+        print("Test checkIn after checkOut failed:", str(e))
 
 
 # Main test scenario
 try:
     make_reservation()
     delete_reservation()
-    # checkIn_after_checkOut()
-    # checkIn_before_currentDate()
+    checkIn_after_checkOut()
 except Exception as e:
     print(f"An error occurred: {str(e)}")
 finally:
